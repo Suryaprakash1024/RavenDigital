350730c92e40bbb821b1a7e79e6a8d63593e20ea
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2023-08-05 11:13:32 +0000

    What we do content

--- src/app/components/what-we-do/what-we-do.component.ts
+++ src/app/components/what-we-do/what-we-do.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-what-we-do',
   templateUrl: './what-we-do.component.html',
   styleUrls: ['./what-we-do.component.scss']
 })
-export class WhatWeDoComponent {
-
+export class WhatWeDoComponent implements OnInit {
+  data : any[] = [];
+  ngOnInit(){
+    this.data = [{title:'APP DEVELOPMENT',content:'Some quick example text to build on the card title and make up the bulk of the card\'s content.'},
+                  {title:'WEBSITE DEVELOPMENT', content:'Some quick example text to build on the card title and make up the bulk of the card\'s content.'},
+                  {title:'UI/UX DESIGNING', content:'Some quick example text to build on the card title and make up the bulk of the card\'s content.'},
+                  {title:'DIGITAL MARKETING', content:'Some quick example text to build on the card title and make up the bulk of the card\'s content.'},
+                  {title:'SEO & CONTENT WRITING', content:'Some quick example text to build on the card title and make up the bulk of the card\'s content.'},
+                  {title:'GRAPHIC DESIGNING', content:'Some quick example text to build on the card title and make up the bulk of the card\'s content.'}];
+  }
 }
