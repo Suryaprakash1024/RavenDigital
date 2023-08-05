13751ec2a6e11196209039824cdb455a071e2e89
Suryaprakash1024 <suryaprakash1024999@gmail.com>
2023-08-05 12:24:25 +0000

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { DigitalAgencyComponent } from './components/digital-agency/digital-agency.component';
 import { WhatWeDoComponent } from './components/what-we-do/what-we-do.component';
+import { OurWorksComponent } from './components/our-works/our-works.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { OurTeamComponent } from './components/our-team/our-team.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { ComponentsComponent } from './components/components.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,14 @@ import { WhatWeDoComponent } from './components/what-we-do/what-we-do.component'
     HeaderComponent,
     FooterComponent,
     DigitalAgencyComponent,
-    WhatWeDoComponent
+    WhatWeDoComponent,
+    OurWorksComponent,
+    TestimonialsComponent,
+    OurTeamComponent,
+    BlogComponent,
+    ClientsComponent,
+    ContactUsComponent,
+    ComponentsComponent
   ],
   imports: [
     BrowserModule,
